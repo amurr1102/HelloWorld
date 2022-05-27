3b7ae48815ea79643c8ea59c0fc96da731fb96d1
aemurr1102 <aemurray1102@gmail.com>
2022-05-27 10:07:07 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("My name is Walter Hartwell White. I live at 308 Negra Arroyo Lane Albuquerque New Mexico 87104.");
